8692fba536b662b93d3c715a3b90b9f3c7c888b2
AlisonJardon <alison.jardon@gmail.com>
2019-11-14 18:59:44 +0100

    first commit

---
+++ script_1.js
@@ -0,0 +1,19 @@
+//2.1.1
+console.log("Bonjour monde")
+
+
+//2.1.2
+
+let word = "";
+let letter;
+
+letter = prompt('Rentre ton prénom stp');
+
+(letter)
+{
+word += letter;
+
+}
+
+console.log(`Bonjour ${word}`);
+
